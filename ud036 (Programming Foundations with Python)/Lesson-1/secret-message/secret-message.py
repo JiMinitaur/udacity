479f02d300389a345e66be8ab753cd3c028ea975
James Arlow <james.arlow@gmail.com>
2015-02-06 11:43:20 -0500

    ud036 : Lesson 1 : secret message : immutable source
    Rather than rename the files, which is hacky and and requires file copies
    or a zipped source package to be reproducible, I used the stripped filenames
    to generate an html page which displays the images in the proper order.
    
    .gitignore is used to keep the output from being stored in the repository.
    
    Signed-off-by: James Arlow <james.arlow@gmail.com>

--- ud036 (Programming Foundations with Python)/Lesson-1/secret-message/secret-message.py
+++ ud036 (Programming Foundations with Python)/Lesson-1/secret-message/secret-message.py
@@ -1,29 +1,80 @@
 import os
 import re
+from operator import itemgetter
 
-imgdir = "prank"
-
-def rename_files():
+def main():
+    imgdir = "prank"
     files = os.listdir(imgdir)
 
-    renamed = []
+    tuples = []
     for file in files:
+        ## get string for modified filename
         newname = stripnum(file)
-        #os.rename(file, newname)
-        renamed.append(newname)
 
-    for file in renamed:
-        print(file)
+        ## lesson recommended path
+        #os.rename(file, newname)
+
+        ## immutable source files option        
+        tuples.append((newname, file))
+
+    ## My first lambda (awwwwww, how cute)
+    ## Sorts the data structure, saving both versions of filename
+    sorteds = sorted(tuples, key=lambda tuple: tuple[0])
+    for tuple in sorteds:
+        print(tuple);
+
+    ## generates html
+    write_output(sorteds)
 
 def stripnum(string):
-    #My solution
+    ## My solution
     rename_regex = "[0-9]*(.*)"
     string = re.search(rename_regex, string).group(1)
 
-    #Suggested solution
+    ## Suggested solution
     #string = string.translate(None, "0123456789")
 
     return string
-    
 
-rename_files();
+def write_output(tuples):
+    sources = "../prank/"
+    location = "output/"
+    target = location + "output.html"
+
+    if not(os.path.isdir(location)):
+        os.mkdir("output")
+    output = open(target,"w")
+    html_open(output)
+
+    for tuple in tuples:
+        output.write("<img src='")
+        output.write(sources)
+        output.write(tuple[1])
+        output.write("""' />
+        """)
+    
+    html_close(output)
+    output.close()
+
+    import webbrowser
+    webbrowser.open(os.path.abspath(target))
+
+def html_open(file):
+    file.write("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>Secret Message</title>
+        <style>img {max-height=100px; max-width:100px}</style>
+        </head>
+        <body>
+        """)
+
+def html_close(file):
+    file.write("""
+        </body>
+        </html>
+        """)
+
+
+main()
