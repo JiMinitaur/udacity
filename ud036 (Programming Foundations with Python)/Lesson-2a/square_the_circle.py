200bf9fd72e8d7845ac141a00701f8c1f9ca4ce6
James Arlow <james.arlow@gmail.com>
2015-02-06 18:39:49 -0500

    ud036 : Lesson-2a : draw a square

--- ud036 (Programming Foundations with Python)/Lesson-2a/square_the_circle.py
+++ ud036 (Programming Foundations with Python)/Lesson-2a/square_the_circle.py
@@ -5,14 +5,22 @@ def main():
     ## b/c exitonclick caused lockups if done before draw
     run_window(draw)
 
-def draw():
-    scrawly = turtle.Turtle()
-    scrawly.forward(100)
+def draw(window):
+    t = build_turtle()
+    length = 100
+    for x in xrange(0,4):
+        t.forward(length)
+        t.left(90)
+
+def build_turtle():
+    """ Pity the fool """
+    mr_t = turtle.Turtle()
+    return mr_t
 
 def run_window(func):
     window = turtle.Screen()
     window.bgcolor("gray")
-    func()
+    func(window) 
     window.exitonclick()
 
 main()
