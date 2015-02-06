d0d6f121a65b10746e2c6677ce4d55f6bd18b410
James Arlow <james.arlow@gmail.com>
2015-02-06 03:00:13 -0500

    ud036 : lesson 1 : knick-knacks
    Added detailed console output and note describing commented line
    
    Signed-off-by: James Arlow <james.arlow@gmail.com>

--- ud036 (Programming Foundations with Python)/take-a-break/open-broswer.py
+++ ud036 (Programming Foundations with Python)/take-a-break/open-broswer.py
@@ -3,6 +3,9 @@ import time
 
 for i in range(0,3):
     time.sleep(3)
-    print("Take a break")
+    print("It is currently " + time.ctime());
+    print("\t Time to take a break")
+
+    #don't bother doing this till the application is closer to completion
     #webbrowser.open("http://www.youtube.com")
 
