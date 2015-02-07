82a031d4e816f2a3e9c51b9dcda8c01ada321c04
James Arlow <james.arlow@gmail.com>
2015-02-07 03:58:20 -0500

    ud036 : Lesson-3a : dynamic property getter/setters
    Signed-off-by: James Arlow <james.arlow@gmail.com>

--- ud036 (Programming Foundations with Python)/Lesson-3a/movie_library.py
+++ ud036 (Programming Foundations with Python)/Lesson-3a/movie_library.py
@@ -1,15 +1,19 @@
+from movies import Movie
 
-import movies
-
-toystory = movies.Movie({
+toystory = Movie({
     'title' : "Toy Story",
     'story' :
 """Adults cry with their children, as inanimate
-objects experience love, adventure, and heartbreak.
+ objects experience love, adventure, and heartbreak.
 """,
     'poster' : "poster link",
     'trailer' : "video link"
     })
 
+print(Movie.list_props())
 
+#test canonical property id
 print(toystory.storyline)
+
+#Test alias property id
+print(toystory.trailer)
--- ud036 (Programming Foundations with Python)/Lesson-3a/movies.py
+++ ud036 (Programming Foundations with Python)/Lesson-3a/movies.py
@@ -1,7 +1,57 @@
 class Movie:
 
+    # Property aliases provide flexibility when defining the data,
+    # while ensuring that only the expected properties are included.
+    property_aliases = {
+        'title' : ['name', 'id']
+    ,   'storyline' : ['story', 'description']
+    ,   'poster_link' : ['poster']
+    ,   'trailer_link' : ['trailer', 'youtube']
+    }
+
+    # Gets the list of canonical property titles
+    @staticmethod
+    def list_props():
+        return Movie.property_aliases.keys()
+
+    # Gets the canonical title of the property alias
+    @staticmethod
+    def deduce_prop(alias):
+        for key, value in Movie.property_aliases.iteritems():
+            if key == alias:
+                return key
+            else:
+                try:
+                    value.index(alias)
+                    return key
+                except:None
+        print("!! property alias not found : "+alias)
+        return None
+
+
     def __init__(self, props):
-        self.title = props['title']
-        self.storyline = props['story']
-        self.poster = props['poster']
-        self.trailer = props['trailer']
+        # An internal dictionary provides dynamic property behavior
+        self.__props = dict()
+        for key, value in props.iteritems():
+            self.set_prop(key, value)
+
+
+    def set_prop(self, key, value):
+        # The canonical key is used to set the value
+        # To prevent rogue entries and searching.
+        alias = Movie.deduce_prop(key)
+        self.__props[alias] = value
+    
+    def __getattr__(self, prop):
+        # For unknown properties, if there is an alias,
+        # retrieve the value with the canonical key
+        alias = Movie.deduce_prop(prop)
+        if alias == None:
+            return None
+        else:
+            return self.__props[alias]
+
+
+    def __str__(self):
+        # Get the string description of the dynamic property container.
+        return str(self.__props)
