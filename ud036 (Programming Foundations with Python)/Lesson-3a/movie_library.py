d660be2aca88a9d4d961cb9bc6dafa015d9debd4
James Arlow <james.arlow@gmail.com>
2015-02-07 03:01:46 -0500

    ud036 : Lesson-3a : basic class module usage
    Signed-off-by: James Arlow <james.arlow@gmail.com>

---
+++ ud036 (Programming Foundations with Python)/Lesson-3a/movie_library.py
@@ -0,0 +1,11 @@
+
+import movies
+
+toystory = movies.Movie(
+    "Toy Story"
+    ,"""Adults cry with their children, as inanimate
+    objects experience love, adventure, and heartbreak."""
+    , "poster link", "video link")
+
+
+print(toystory.storyline)
---
+++ ud036 (Programming Foundations with Python)/Lesson-3a/movies.py
@@ -0,0 +1,9 @@
+class Movie:
+
+    def __init__(self, title, story, poster, trailer):
+        self.title = title
+        self.storyline = story
+        self.poster = poster
+        self.trailer = trailer
+
+ 
